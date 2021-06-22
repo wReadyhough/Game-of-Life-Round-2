30016f0dae88a1372315259032e126c3fdd3f88d
willReadyhough <66326631+wReadyhough@users.noreply.github.com>
2021-06-22 13:24:09 +0000

    Rewritten Update up to eat().

--- main.cpp
+++ main.cpp
@@ -3,11 +3,29 @@
 
 
 int main(){
-  Predator myPredator;
-  Prey myPrey;
-  cout<<"A predator starts with a health of "<<myPredator.getHealth()<<endl;
-  cout<<"A predator has a color of "<<myPredator.getColor()<<endl;
-  cout<<"A prey starts with a health of "<<myPrey.getHealth()<<endl;
-  cout<<"A prey starts with a color of "<<myPrey.getColor()<<endl;
+  srand(time(NULL));
+  int rows = 5, cols = 5, numPred = 6, numPrey = 8;
+  cout<<"Making board with "<<rows<<" rows, "<<cols<<" cols, "<<numPred<<" predators, and "<<numPrey<<" prey."<<endl;
+  Board theBoard(rows, cols, numPred, numPrey);
+  cout<<"Board looks like: "<<endl;
+  theBoard.printBoard();
+  cout<<"Updating board..."<<endl;
+  theBoard.update();
+  theBoard.printBoard();
+  
+
+
+  
+
   return 0;
 }
+
+/*while(theBoard.numPrey() > 0 && theBoard.numPreys() > 0){
+    //usleep(1000000);
+    //cin.get();
+    cout<<"printing..."<<endl;
+    theBoard.printBoard();
+    cout<<"\n\n\n\n";
+    cout<<"Updating..."<<endl;
+    theBoard.update();
+  }*/
--- parent.cpp
+++ parent.cpp
@@ -1,5 +1,12 @@
 #include "parent.h"
-
+//0/O is Prey 1/X is Predator
+//------Parent Stuff------//
+Parent::~Parent(){
+  m_color = "SHOULD BE DEAD.";
+  m_health = -100;
+  m_myX = -10;
+  m_myY = -10;
+}
 int Parent::getHealth(){
   return m_health;
 }
@@ -16,12 +23,319 @@ void Parent::setColor(string color){
   m_color = color;
 }
 
-Predator::Predator(){
-  setColor("Red");
-  setHealth(START_PREDATOR_HEALTH);
+void Parent::setCoords(int row, int col){
+  m_myX = col;
+  m_myY = row;
 }
 
-Prey::Prey(){
+int Parent::getRow(){
+  return m_myY;
+}
+
+int Parent::getCol(){
+  return m_myX;
+}
+
+
+//------Predator Stuff------//
+Predator::Predator(int row, int col){
+  setColor("Red");
+  setHealth(START_PREDATOR_HEALTH);
+  setCoords(row, col);
+}
+
+//returns true if now dead.
+//passed the health of the prey it just ate, or 0 for didn't eat.
+bool Predator::updateHealth(int healthIncrease){
+  // cout<<"In predator update health."<<endl;
+  //Has a health increase
+  int myHealth = this->getHealth();
+  // cout<<"current health is: "<<myHealth<<endl;
+  //Ate, change health.
+  if(healthIncrease){
+    // cout<<"Increasing health by "<<healthIncrease<<endl;
+    setHealth(myHealth + healthIncrease);
+  }
+  //Didn't eat. Decrease health by one and check if dead
+  else{
+    // cout<<"Decreasing health by 1"<<endl;
+      setHealth(myHealth -1);
+  }
+
+  //check if dead.
+  //is not dead
+  // cout<<"New health is: "<<getHealth()<<endl;
+  if(getHealth() > 0){
+    return false;
+  }
+  return true;
+}
+
+
+
+//------Prey Stuff------//
+Prey::Prey(int row, int col){
   setColor("Green");
   setHealth(START_PREY_HEALTH);
+  setCoords(row, col);
+}
+
+void Prey::updateHealth(bool reproduced){
+  if(reproduced){
+    setHealth(START_PREY_HEALTH);
+  }
+  else{
+    setHealth(getHealth() + 1);
+  }
+}
+
+//Returns true if needs to reproduce
+bool Prey::reproduce(){
+  return(getHealth() >= PREY_REPRODUCTION_THRESHOLD);
+}
+
+//0:Prey, 1: Predator, 2: Nothing
+//------Board Stuff------//
+Board::Board(int col, int row, int numPredator, int numPrey){
+  m_xDim = col;
+  m_yDim = row;
+  // cout<<"creating board..."<<endl;
+  createBoard();
+  // cout<<"Adding predators and prey..."<<endl;
+  addPredPrey(numPredator, numPrey);
+}
+
+void Board::createBoard(){
+  vector<int> temp;
+  for(int x = 0; x <m_xDim; x++){
+    temp.push_back(2);
+  }
+  for(int y = 0; y < m_yDim; y++){
+    m_board.push_back(temp);
+  } 
+  cout<<"Board created with "<<m_board.size()<<" rows and "<<m_board[0].size()<< " cols"<<endl;
+
+}
+
+void Board::addPredPrey(int numPredator, int numPrey){
+  int row, col;
+  //Enter predators
+  while(numPredator > 0){
+    row = rand() % m_yDim;
+    col = rand() % m_xDim;
+    //Nothing there yet, enter a predator.
+    if(m_board[row][col] == 2){
+      Predator tempPred(row, col);
+      m_predators.push_back(tempPred);
+      m_board[row][col] = 1;
+      numPredator--;
+    }
+  }
+  while(numPrey > 0){
+    row = rand() % m_yDim;
+    col = rand() % m_xDim;
+    if(m_board[row][col] == 2){
+      Prey tempPrey(row, col);
+      m_prey.push_back(tempPrey);
+      m_board[row][col] = 0;
+      numPrey--;
+    }
+  }
+  cout<<"Board now has "<<m_predators.size()<<" predators and "<<m_prey.size()<<" prey."<<endl;
+}
+
+//Predators desiplayed as X, Prey as O, nothing as -
+void Board::printBoard(){
+  //system("clear");
+  cout<<"Prey: O, Predators: X"<<endl;
+  for(int y = 0; y < m_yDim; y++){
+    for(int x = 0; x < m_xDim; x++){
+      switch(m_board[y][x]){
+        case 0: //Prey
+          cout<<'O';
+          break;
+        case 1: //Predator
+          cout<<'X';
+          break;
+        case 2: //Nothing
+          cout<<'-';
+          break;
+        default:
+          cout<<"Ummmm WHAT???"<<endl;
+          break;
+      }
+    }
+    cout<<endl;
+  }
+}
+
+void Board::update(){
+  //News hold where predator/prey will move to. Prevs hold where it was.
+  int prevRow = 0, newRow = 0, prevCol = 0,  newCol = 0;
+  bool canMove = false, canEat = false;
+  int currPredSize = 0;//Restricts new predators from eating.
+  int preyHealth = 0;
+
+
+  //For now, have newly born prey be initalized wherever. 
+  //Later, initialize them where the parent moved from if possible
+  cout<<"Moving prey..."<<endl;
+  for(int i = 0; i < m_prey.size(); i++){
+    
+    prevRow = m_prey[i].getRow();
+    prevCol = m_prey[i].getCol();
+    newRow = prevRow;
+    newCol = prevCol;
+
+    canMove = move(newRow, newCol);
+
+    //Prey Can Move.
+    if(canMove == true){
+      m_board[prevRow][prevCol] = 2;
+      m_board[newRow][newCol] = 0;
+      m_prey[i].setCoords(newRow,newCol);
+    }
+    //Update all prey health
+    m_prey[i].updateHealth(false);
+
+  }
+
+  //All prey that can give birth, now do so.
+  //Cycle through for the size of the vector.
+  //This will make us go through any new borns but, it doesnt matter since they can't give birth yet.
+  cout<<"The Baby Boom..."<<endl;
+  for(int i = 0; i <m_prey.size(); i++){
+    if(m_prey[i].reproduce()){
+      m_prey[i].updateHealth(true);
+      addPredPrey(0,1);
+    }
+  }
+
+
+  cout<<"Moving Predators..."<<endl;
+  for(int i = 0; i < m_predators.size(); i++){
+    
+    prevRow = m_predators[i].getRow();
+    prevCol = m_predators[i].getCol();
+    newRow = prevRow;
+    newCol = prevCol;
+    canMove = move(newRow, newCol);
+
+    //Predator can move
+    if(canMove == true){
+      m_board[prevRow][prevCol] = 2;
+      m_board[newRow][newCol] = 1;
+      m_predators[i].setCoords(newRow,newCol);
+    }
+
+  }
+
+  cout<<"The Great Massacre..."<<endl;
+  currPredSize = m_predators.size();
+  for(int i =0; i < currPredSize; i++){
+
+    newRow = m_predators[i].getRow();
+    newCol = m_predators[i].getCol();
+    //news will hold the location of the prey to be eaten, in the event that true is returned
+    canEat = eat(newRow, newCol);
+
+    //Predator has a prey to eat.
+    if(canEat == true){
+      //Putting a new predator at the prey's location.
+      m_board[newRow][newCol] = 1;
+      m_predators.push_back(Predator(newRow,newCol));
+
+      
+    }
+  }
+  cout<<"Finished updating with "<<m_predators.size()<<" predators and "<<m_prey.size()<<" prey."<<endl;
+}
+
+//If it finds an open space, sets pointers to the coords of that space. 
+//If not, pointers should still point to prey's initial spot
+//void Board::preyMove(int *row, int *col){
+  bool Board::move(int &row, int &col){
+  //int r = *row;
+  //int c = *col;
+  int r = row;
+  int c = col;
+  for(int y = -1; y <2;y++ ){
+    for(int x = -1; x < 2; x++){
+      if((y+r) < 0 || (y+r) >= m_yDim ){
+        /*cout<<"skipping row "<<y+r;
+        if((y + r) < 0){
+          cout<<" --> "<<y<<" + "<<r<<" < 0"<<endl;
+        }
+        else if((y + r) >= m_yDim){
+          cout<<" --> "<<y<<" + "<<r<<" >= "<<m_yDim<<endl;
+        }
+        else{
+          cout<<"what the fuck row"<<endl;
+        }*/
+        continue;
+      }
+      if((x+c)<0 || (x+c) >= m_xDim){
+        /*cout<<"skipping col "<<x+c;
+        if((x + c) < 0){
+          cout<<" --> "<<x<<" + "<<c<<" < 0"<<endl;
+        }
+        else if((x + c) >= m_xDim){
+          cout<<" --> "<<x<<" + "<<c<<" >= "<<m_xDim<<endl;
+        }
+        else{
+          cout<<"what the fuck col"<<endl;
+        }*/
+        continue;
+      }
+      if(m_board[y+r][x+c] == 2){
+        row = y + r;
+        col = x + c;
+        //cout<<"row "<<row<<" col "<<col<<endl;
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+//If the predator has a prey available for consumption,
+//Changes the arguments to the prey's location and returns true
+//Otherwise, returns false.
+//Literally just the move function implemented to look for 0 instead of 2.
+//Can definitely combine the two into one function later.
+bool Board::eat(int &predRow, int &predCol){
+  int r = row;
+  int c = col;
+  for(int y = -1; y <2;y++ ){
+    for(int x = -1; x < 2; x++){
+      if((y+r) < 0 || (y+r) >= m_yDim ){
+        continue;
+      }
+      if((x+c)<0 || (x+c) >= m_xDim){
+        continue;
+      }
+      if(m_board[y+r][x+c] == 0){
+        row = y + r;
+        col = x + c;
+        return true;
+      }
+    }
+  }
+  return false;
+
+
+  return 0;
+}
+
+//int Board::removePrey(int row, int col){
+int Board::removePrey(int preyIndex){
+  return 0;
+}
+
+int Board::numPrey(){
+  return m_prey.size();
+}
+
+int Board::numPredators(){
+  return m_predators.size();
 }
